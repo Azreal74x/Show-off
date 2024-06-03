67b97fd0dcfe1e9c38287901138d9c305d1c3130
Emily74x <emilymurad2016@gmail.com>
2024-06-03 17:49:03 +0200

    Button sounds
    Still have to add Jemie model and more sounds

--- Show-off/Assets/Scripts/ButtonScripts.cs
+++ Show-off/Assets/Scripts/ButtonScripts.cs
@@ -5,129 +5,119 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class ButtonScripts : MonoBehaviour
-{
-    [SerializeField] private string houseScene;
+public class ButtonScripts : MonoBehaviour {
+  [SerializeField] private string houseScene;
 
-    [SerializeField] private string F_gameName;
-    [SerializeField] private string J_gameName;
-    [SerializeField] private string T_gameName;
+  [SerializeField] private string F_gameName;
+  [SerializeField] private string J_gameName;
+  [SerializeField] private string T_gameName;
 
-    [SerializeField] private string F_roomName;
-    [SerializeField] private string J_roomName;
-    [SerializeField] private string T_roomName;
+  [SerializeField] private string F_roomName;
+  [SerializeField] private string J_roomName;
+  [SerializeField] private string T_roomName;
 
-    [SerializeField] private string F_gameover;
-    [SerializeField] private string J_gameover;
-    [SerializeField] private string T_gameover;
+  [SerializeField] private string F_gameover;
+  [SerializeField] private string J_gameover;
+  [SerializeField] private string T_gameover;
 
 
-    void Start()
-    {
+
+  [SerializeField] private AudioClip buttonClickSound;
+  private AudioSource audioSource;
+
+  void Start() {
+    audioSource = GetComponent<AudioSource>();
+  }
+
+  void Update() {
+
+  }
+
+  public void LoadMainHouseScene() {
+    SceneManager.LoadScene(houseScene);
+  }
+
+  //specifc methods for each game
+  public void F_LoadGame() {
+    SceneManager.LoadScene(F_gameName);
+  }
+  public void T_LoadGame() {
+    SceneManager.LoadScene(T_gameName);
+  }
+  public void J_LoadGame() {
+    SceneManager.LoadScene(J_gameName);
+  }
+
+  //specific method for each house click
+  public void F_LoadHouse() {
+    SceneManager.LoadScene("HouseFurbie");
+  }
+  public void T_LoadHouse() {
+    SceneManager.LoadScene("HouseTamara");
+  }
+  public void J_LoadHouse() {
+    SceneManager.LoadScene("HouseJemie");
+  }
+
+  //depending in what scene u are, it goes to the coresponding next scene
+  public void Replay() {
+    if (SceneManager.GetActiveScene().name == F_gameover) {
+      SceneManager.LoadScene(F_gameName);
+    }
+    else
+
+    if (SceneManager.GetActiveScene().name == J_gameover) {
+      SceneManager.LoadScene(J_gameName);
+    }
+    else
+
+    if (SceneManager.GetActiveScene().name == T_gameover) {
+      SceneManager.LoadScene(T_gameName);
     }
 
-    void Update()
-    {
+  }
+
+
+  public void BackToRoom() {
+    if (SceneManager.GetActiveScene().name == F_gameover) {
+      SceneManager.LoadScene(F_roomName);
+    }
+    else
+
+    if (SceneManager.GetActiveScene().name == J_gameover) {
+      SceneManager.LoadScene(J_roomName);
+    }
+    else
+
+    if (SceneManager.GetActiveScene().name == T_gameover) {
+      SceneManager.LoadScene(T_roomName);
     }
 
-    public void LoadMainHouseScene() 
-    {
-        SceneManager.LoadScene(houseScene);
+  }
+
+
+
+  public void GameOver() {
+    if (SceneManager.GetActiveScene().name == F_gameName) {
+      SceneManager.LoadScene(F_gameover);
+    }
+    else
+
+    if (SceneManager.GetActiveScene().name == J_gameName) {
+      SceneManager.LoadScene(J_gameover);
+    }
+    else
+
+    if (SceneManager.GetActiveScene().name == T_gameName) {
+      SceneManager.LoadScene(T_gameover);
     }
 
-    //specifc methods for each game
-    public void F_LoadGame()
-    {
-        SceneManager.LoadScene(F_gameName);
-    }
-    public void T_LoadGame()
-    {
-        SceneManager.LoadScene(T_gameName);
-    }
-    public void J_LoadGame()
-    {
-        SceneManager.LoadScene(J_gameName);
-    }
+  }
 
-    //specific method for each house click
-    public void F_LoadHouse()
-    {
-        SceneManager.LoadScene("HouseFurbie");
-    }
-    public void T_LoadHouse()
-    {
-        SceneManager.LoadScene("HouseTamara");
-    }
-    public void J_LoadHouse()
-    {
-        SceneManager.LoadScene("HouseJemie");
-    }
-
-    //depending in what scene u are, it goes to the coresponding next scene
-    public void Replay()
-    {
-        if(SceneManager.GetActiveScene().name == F_gameover)
-        {
-                SceneManager.LoadScene(F_gameName);
-        }else
-
-        if (SceneManager.GetActiveScene().name == J_gameover)
-        {
-            SceneManager.LoadScene(J_gameName);
-        }else
-
-        if (SceneManager.GetActiveScene().name == T_gameover)
-        {
-            SceneManager.LoadScene(T_gameName);
-        }
-
-    }
-
-
-    public void BackToRoom()
-    {
-        if(SceneManager.GetActiveScene().name == F_gameover)
-        {
-                SceneManager.LoadScene(F_roomName);
-        }else
-
-        if (SceneManager.GetActiveScene().name == J_gameover)
-        {
-            SceneManager.LoadScene(J_roomName);
-        }
-        else
-
-        if (SceneManager.GetActiveScene().name == T_gameover)
-        {
-            SceneManager.LoadScene(T_roomName);
-        }
-        
-    }
-
-
-
-    public void GameOver()
-    {
-        if (SceneManager.GetActiveScene().name == F_gameName)
-        {
-            SceneManager.LoadScene(F_gameover);
-        }
-        else
-
-        if (SceneManager.GetActiveScene().name == J_gameName)
-        {
-            SceneManager.LoadScene(J_gameover);
-        }
-        else
-
-        if (SceneManager.GetActiveScene().name == T_gameName)
-        {
-            SceneManager.LoadScene(T_gameover);
-        }
-        
-    }
-
-    
+  public void PlayButtonSound() {
+    audioSource.clip = buttonClickSound;
+    audioSource.Play();
+  }
 
 
 
--- Show-off/Assets/Scripts/GameMan.cs
+++ Show-off/Assets/Scripts/GameMan.cs
@@ -3,56 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameMan : MonoBehaviour
-{
-    public static GameMan currentInstance;
+public class GameMan : MonoBehaviour {
+  public static GameMan currentInstance;
 
 
-    void Awake()
-    {
-        if (currentInstance != null)
-        {
-            Destroy(currentInstance);
-            currentInstance = this;
-        }
-        else
-        {
-            currentInstance = this;
-        }
+  void Awake() {
+    if (currentInstance != null) {
+      Destroy(currentInstance);
+      currentInstance = this;
     }
-
-    void Start()
-    {
-
+    else {
+      currentInstance = this;
     }
+  }
 
-    void Update()
-    {
+  void Start() {
 
-    }
+  }
 
-    /*public void BackButtonAction()
-    {
-        //SceneManager.SetActiveScene(SceneManager.GetSceneByName("Map"));
-        SceneManager.LoadScene("HomeScene"*//*, LoadSceneMode.Additive*//*);
-    }
-    public void LoadHouse1()
-    {
-        SceneManager.LoadScene("HouseFurbie"*//*, LoadSceneMode.Additive*//*);
-    }
-    public void LoadHouse2()
-    {
-        SceneManager.LoadScene("HouseTamara"*//*, LoadSceneMode.Additive*//*);
-    }
-    public void LoadHouse3()
-    {
-        SceneManager.LoadScene("HouseJemie"*//*, LoadSceneMode.Additive*//*);
-    }
+  void Update() {
 
-    public void LoadFurbieGame()
-    {
-        SceneManager.LoadScene("FlightTraining");
-    }*/
+  }
+
+  /*public void BackButtonAction()
+  {
+      //SceneManager.SetActiveScene(SceneManager.GetSceneByName("Map"));
+      SceneManager.LoadScene("HomeScene"*//*, LoadSceneMode.Additive*//*);
+  }
+  public void LoadHouse1()
+  {
+      SceneManager.LoadScene("HouseFurbie"*//*, LoadSceneMode.Additive*//*);
+  }
+  public void LoadHouse2()
+  {
+      SceneManager.LoadScene("HouseTamara"*//*, LoadSceneMode.Additive*//*);
+  }
+  public void LoadHouse3()
+  {
+      SceneManager.LoadScene("HouseJemie"*//*, LoadSceneMode.Additive*//*);
+  }
+
+  public void LoadFurbieGame()
+  {
+      SceneManager.LoadScene("FlightTraining");
+  }*/
 
 
 
