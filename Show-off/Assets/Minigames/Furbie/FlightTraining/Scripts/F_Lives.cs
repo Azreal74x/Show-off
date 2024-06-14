910568eff12c2f777a32b9910144ac9658061b87
Emily74x <emilymurad2016@gmail.com>
2024-06-14 03:04:04 +0200

    Change UI up a bit
    In progress!

--- Show-off/Assets/Minigames/Furbie/FlightTraining/Scripts/F_Lives.cs
+++ Show-off/Assets/Minigames/Furbie/FlightTraining/Scripts/F_Lives.cs
@@ -2,29 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class F_Lives : MonoBehaviour
-{
-    [SerializeField] public int lives = 5;
+public class F_Lives : MonoBehaviour {
+  [SerializeField] public int lives = 5;
+  [SerializeField] BoolSO F_IsHappy;
 
-    void Start()
-    {
+  void Start() {
 
+  }
+
+  // Update is called once per frame
+  void Update() {
+    if (lives == 0) {
+      lives = -1;
+
+      Debug.Log("GAMEOVER");
+
+      F_IsHappy.Value = true;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (lives == 0)
-        {
-            lives = -1;
-
-            Debug.Log("GAMEOVER");
-
-
-
-
-        }
-    }
+  }
 
 
 
