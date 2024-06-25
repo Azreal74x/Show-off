687ac5b54cecb7cb1fab6d75a930176a857a5aec
morqp <mara.cosneanu@yahoo.com>
2024-06-25 14:32:55 +0200

    hand cursor scripts in
    just have to put them on the things
    (laptop abt to die)

--- Show-off/Assets/Houses/Scripts/SwitchWallObjects.cs
+++ Show-off/Assets/Houses/Scripts/SwitchWallObjects.cs
@@ -2,39 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwitchWallObjects : MonoBehaviour {
-  [SerializeField] private List<GameObject> wallObjects = new List<GameObject>(); // list of objects
-  private int activeObjIndex = -1;
+public class SwitchWallObjects : Clickable
+{
+    [SerializeField] private List<GameObject> wallObjects = new List<GameObject>(); // list of objects
+    private int activeObjIndex = -1;
 
-  [SerializeField] private SoundManager soundManager;
+    [SerializeField] private SoundManager soundManager;
 
-  void Start() {
-    if (activeObjIndex == -1) // do this only the first time
+    void Start()
     {
-      for (int i = 0; i < wallObjects.Count; i++) //turn off all objects
-      {
-        wallObjects[i].SetActive(false);
-      }
+        if (activeObjIndex == -1) // do this only the first time
+        {
+            for (int i = 0; i < wallObjects.Count; i++) //turn off all objects
+            {
+                wallObjects[i].SetActive(false);
+            }
 
-      wallObjects[0].SetActive(true); // turn on only first one
-      activeObjIndex = 0;
+            wallObjects[0].SetActive(true); // turn on only first one
+            activeObjIndex = 0;
 
+        }
     }
-  }
 
-  void Update() {
-  }
+    void Update()
+    {
+    }
 
-  private void OnMouseUp() {
-    // disable the current
-    wallObjects[activeObjIndex].SetActive(false);
+    private void OnMouseUp()
+    {
+        // disable the current
+        wallObjects[activeObjIndex].SetActive(false);
 
-    // +1 the index and make sure it cycles
-    activeObjIndex = (activeObjIndex + 1) % wallObjects.Count;
+        // +1 the index and make sure it cycles
+        activeObjIndex = (activeObjIndex + 1) % wallObjects.Count;
 
-    wallObjects[activeObjIndex].SetActive(true);
+        wallObjects[activeObjIndex].SetActive(true);
 
-    soundManager.PlayChangeObjectSound();
-  }
+        soundManager.PlayChangeObjectSound();
+    }
 
 }
