48c6c34dd1e27cf02e791c0021148975e69be332
Emily74x <emilymurad2016@gmail.com>
2024-06-28 09:25:01 +0200

    Deleted all unused files and fixed small bugs
    Big project clean up and fixed food feeding bug and background music low sound

--- Show-off/Assets/FINAL/Scripts/MoveFood.cs
+++ Show-off/Assets/FINAL/Scripts/MoveFood.cs
@@ -2,40 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveFood : MonoBehaviour
-{
-    //public GameObject ballPrefab; // The ball prefab to spawn
-    public Transform target; // The target position to move to
-    [SerializeField] private float duration = 1.0f; // Duration of the movement
-    //public Button spawnButton; // Button to spawn the balls
+public class MoveFood : MonoBehaviour {
+  public Transform target; // The target position to move to
+  [SerializeField] private float duration = 1.0f; // Duration of the movement
+  private bool isMoving = false; // Flag to prevent spamming
 
-    void Start()
-    {
-        
+  void Start() {
+    // Initialize if necessary
+  }
+
+  public void StartMovement() {
+    if (isMoving) return; // Prevent multiple instances if already moving
+
+    GameObject newFood = Instantiate(this.gameObject, transform);
+    StartCoroutine(MoveToPosition(newFood, target.position, duration));
+  }
+
+  System.Collections.IEnumerator MoveToPosition(GameObject food, Vector3 target, float duration) {
+    isMoving = true; // Set flag to true to indicate movement is in progress
+    RectTransform rectTransform = food.GetComponent<RectTransform>();
+
+    Vector3 startPosition = rectTransform.position;
+    float elapsedTime = 0;
+
+    while (elapsedTime < duration) {
+      rectTransform.position = Vector3.Lerp(startPosition, target, elapsedTime / duration);
+      elapsedTime += Time.deltaTime;
+      yield return null;
     }
 
-    public void StartMovement()
-    {
-        GameObject newFood = Instantiate(this.gameObject, transform);
-        StartCoroutine(MoveToPosition(newFood, target.position, duration));
-    }
-
-    System.Collections.IEnumerator MoveToPosition(GameObject food, Vector3 target, float duration)
-    {
-        RectTransform rectTransform = food.GetComponent<RectTransform>();
-        //rectTransform.localScale = this.transform.localScale;
-        
-        Vector3 startPosition = rectTransform.position;
-        float elapsedTime = 0;
-
-        while (elapsedTime < duration)
-        {
-            rectTransform.position = Vector3.Lerp(startPosition, target, elapsedTime / duration);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        rectTransform.position = target;
-        Destroy(food);
-    }
+    rectTransform.position = target;
+    Destroy(food);
+    isMoving = false; // Reset flag after movement is complete
+  }
 }
--- Show-off/Assets/Houses/Scripts/SwitchWallObjects.cs
+++ Show-off/Assets/Houses/Scripts/SwitchWallObjects.cs
@@ -2,43 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwitchWallObjects : Clickable
-{
-    [SerializeField] private List<GameObject> wallObjects = new List<GameObject>(); // list of objects
-    private int activeObjIndex = -1;
+public class SwitchWallObjects : Clickable {
+  [SerializeField] private List<GameObject> wallObjects = new List<GameObject>(); // list of objects
+  private int activeObjIndex = -1;
 
-    [SerializeField] private SoundManager soundManager;
+  [SerializeField] private SoundManager soundManager;
+  [SerializeField] private GameObject catBoard;
 
-    void Start()
+  void Start() {
+    if (activeObjIndex == -1) // do this only the first time
     {
-        if (activeObjIndex == -1) // do this only the first time
-        {
-            for (int i = 0; i < wallObjects.Count; i++) //turn off all objects
-            {
-                wallObjects[i].SetActive(false);
-            }
+      for (int i = 0; i < wallObjects.Count; i++) //turn off all objects
+      {
+        wallObjects[i].SetActive(false);
+      }
 
-            wallObjects[0].SetActive(true); // turn on only first one
-            activeObjIndex = 0;
+      wallObjects[0].SetActive(true); // turn on only first one
+      activeObjIndex = 0;
 
-        }
     }
+  }
 
-    void Update()
-    {
+  void Update() {
+  }
+
+  private void OnMouseUp() {
+    if (!catBoard.activeSelf) {
+      // disable the current
+      wallObjects[activeObjIndex].SetActive(false);
+
+      // +1 the index and make sure it cycles
+      activeObjIndex = (activeObjIndex + 1) % wallObjects.Count;
+
+      wallObjects[activeObjIndex].SetActive(true);
+
+      soundManager.PlayChangeObjectSound();
     }
-
-    private void OnMouseUp()
-    {
-        // disable the current
-        wallObjects[activeObjIndex].SetActive(false);
-
-        // +1 the index and make sure it cycles
-        activeObjIndex = (activeObjIndex + 1) % wallObjects.Count;
-
-        wallObjects[activeObjIndex].SetActive(true);
-
-        soundManager.PlayChangeObjectSound();
-    }
+  }
 
 }
